c77ef7209396d6215a729d65a05d0d1a50dfc14a
salcudeann <adi.salcudeann@yahoo.com>
2021-07-29 16:42:15 +0300

    Create custom Uri type

---
+++ app/graphql/types/url.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Types
+  class Url < Types::BaseScalar
+    description 'A valid URL, transported as a string'
+
+    def self.coerce_input(input_value, _context)
+      url = URI.parse(input_value)
+      return if url.is_a?(URI::HTTP) || url.is_a?(URI::HTTPS)
+    end
+
+    def self.coerce_result(ruby_value, _context)
+      ruby_value.to_s
+    end
+  end
+end

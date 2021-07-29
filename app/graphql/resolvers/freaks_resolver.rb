dce3dce2b161d8c8aec89f6322d8a093c551890f
salcudeann <adi.salcudeann@yahoo.com>
2021-07-29 16:47:37 +0300

    Hard coded data for Freak model

--- app/graphql/resolvers/freaks_resolver.rb
+++ app/graphql/resolvers/freaks_resolver.rb
@@ -9,14 +9,96 @@ module Resolvers
         {
           id: '1',
           name: 'Doru Pirvu',
-          photo: 'https://bit.ly/3y7afHO',
-          description: 'The boss.'
+          photo: {
+            id: '1',
+            uri: 'https://bit.ly/3y7afHO'
+          },
+          description: 'The boss.',
+          skills: [{
+            id: '1',
+            name: 'Project manangement'
+          },
+                   {
+                     id: '2',
+                     name: 'AWS Architect'
+                   }],
+          projects: [{
+            id: '1',
+            name: 'EPIX',
+            description: 'An American premium cable and satellite TV network.',
+            photo: {
+              id: '1',
+              uri: 'https: //bit.ly/3y7afHO'
+            },
+            technologies: [{
+              id: '2',
+              name: 'Ruby',
+              description: 'A popular backend language.'
+            }]
+          }],
+          role: {
+            id: '1',
+            name: 'Founder'
+          },
+          level: {
+            id: '3',
+            name: 'Advanced'
+          },
+          norm: {
+            id: '1',
+            name: 'full-time'
+          },
+          speciality: [{
+            id: '2',
+            name: 'Backend'
+          }]
         },
         {
           id: '2',
           name: 'Alex Negru',
-          photo: 'https://bit.ly/3y7afHO',
-          description: 'Intern driver.'
+          photo: {
+            id: '1',
+            uri: 'https://bit.ly/3y7afHO'
+          },
+          description: 'Intern driver.',
+          skills: [{
+            id: '1',
+            name: 'Backend developer'
+          },
+                   {
+                     id: '2',
+                     name: 'driver'
+                   }],
+          projects: [{
+            id: '1',
+            name: 'EPIX',
+            description: 'An American premium cable and satellite TV network.',
+            photo: {
+              id: '1',
+              uri: 'https://bit.ly/3rB9Yu9'
+            },
+            technologies: [{
+              id: '2',
+              name: 'Ruby',
+              description: 'A popular backend language.'
+            }]
+          }],
+          role: {
+            id: '1',
+            name: 'Software developer'
+          },
+          level: {
+            id: '3',
+            name: 'Advanced'
+          },
+          norm: {
+            id: '1',
+            name: 'full-time'
+          },
+          speciality: [{
+            id: '2',
+            name: 'Backend'
+          }]
         }
       ]
     end

28df6b10a7a881d07ed97a77cc842d77fdb5f014
gabrielgiroe <gabriel.giroe@gmail.com>
2021-08-02 14:54:35 +0300

    Create test for graphql mutation

--- app/graphql/resolvers/create_freak_resolver.rb
+++ app/graphql/resolvers/create_freak_resolver.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 module Resolvers
   class CreateFreakResolver < Resolvers::Base
-
     argument :name, String, required: true
     argument :description, String, required: true
     type [Types::FreakType], null: false

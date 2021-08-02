3e86eb1f8abad629f9e4fe0e078e8f2f95e0c69a
gabrielgiroe <gabriel.giroe@gmail.com>
2021-08-02 17:31:18 +0300

    [116] Add Freaks schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_802_074_146) do
-  create_table 'freaks', force: :cascade do |t|
-    t.string 'name'
-    t.string 'description'
-    t.date 'photo'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+ActiveRecord::Schema.define(version: 2021_08_02_074146) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "freaks", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.date "photo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
 end

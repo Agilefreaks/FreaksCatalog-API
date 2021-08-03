1744f30194fc9d5e76b55fba9664c33a118476ad
salcudeann <adi.salcudeann@yahoo.com>
2021-08-03 14:46:37 +0300

    [128] Fix RSpec

--- spec/controllers/graphql/queries/freaks_spec.rb
+++ spec/controllers/graphql/queries/freaks_spec.rb
@@ -12,6 +12,6 @@ module Graphql
       }
     end
 
-    it { is_expected.to match_response_for(query: :freaks, sample: :default) }
+    it { is_expected.to match_response_for(query: :Freaks, sample: :default) }
   end
 end

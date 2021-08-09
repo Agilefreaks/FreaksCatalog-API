3d08d903586be4d7796965581533f96438562c09
salcudeann <adi.salcudeann@yahoo.com>
2021-08-09 16:31:59 +0300

    [] Fix rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -22,5 +22,4 @@ ActiveRecord::Schema.define(version: 20_210_804_115_545) do
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
-
 end

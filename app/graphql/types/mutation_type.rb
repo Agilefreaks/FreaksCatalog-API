afd903c5930d7417092602b4cf8447001f911ef5
salcudeann <adi.salcudeann@yahoo.com>
2021-08-10 11:57:41 +0300

    [130] Add a mutation type to create a photo entry

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :freak_create, FreakType, null: false, resolver: Resolvers::FreakCreateResolver
+    field :photo_create, Models::Photo, null: false, resolver: Resolvers::PhotoCreateResolver
   end
 end

b00fca2f57d2c7917c1f1eed4094d8a9c5786a16
salcudeann <adi.salcudeann@yahoo.com>
2021-08-11 14:38:43 +0300

    [121] Add migration to create project table in database

---
+++ db/migrate/20210811084757_create_projects.rb
@@ -0,0 +1,11 @@
+class CreateProjects < ActiveRecord::Migration[6.1]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.string :description
+      t.references :proj, polymorphic: true
+
+      t.timestamps
+    end
+  end
+end

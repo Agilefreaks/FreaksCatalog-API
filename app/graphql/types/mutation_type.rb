5806491d05d9028e898a85c08060a2ddf536ea0c
salcudeann <adi.salcudeann@yahoo.com>
2021-08-11 14:37:12 +0300

    [121] Create a mutation for project

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
   class MutationType < BaseObject
     field :freak_create, FreakType, null: false, resolver: Resolvers::FreakCreateResolver
     field :photo_create, Models::Photo, null: false, resolver: Resolvers::PhotoCreateResolver
+    field :project_create, Models::Project, null: false, resolver: Resolvers::ProjectCreateResolver
   end
 end

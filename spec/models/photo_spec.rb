cb15f146f6a82a61b127316dab0cf8936598f0ea
salcudeann <adi.salcudeann@yahoo.com>
2021-08-16 10:43:22 +0300

    [130] Fix naming issue

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
-  describe '#name' do
+  describe '#uri' do
     subject(:uri) { described_class.create(uri: 'http://www.ceva.ro').uri }
 
     it { is_expected.to eq 'http://www.ceva.ro' }

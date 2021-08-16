9674f614a876bf8a7d18705159feeec184b4feb4
salcudeann <adi.salcudeann@yahoo.com>
2021-08-16 16:04:16 +0300

    [] Update schema for freaks table

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_802_074_146) do
+ActiveRecord::Schema.define(version: 20_210_816_125_701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
   create_table 'freaks', force: :cascade do |t|
-    t.string 'first_name'
-    t.string 'last_name'
     t.string 'description'
-    t.string 'email'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.string 'first_name'
+    t.string 'last_name'
+    t.string 'email'
   end
 end

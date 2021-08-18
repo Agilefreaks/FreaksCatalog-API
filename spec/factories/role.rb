0a2a7868f2563c65fe1c1feb13df780d28215430
gabrielgiroe <gabriel.giroe@gmail.com>
2021-08-18 11:10:29 +0300

    [118] Refactoring factory bot to create a role

--- spec/factories/role.rb
+++ spec/factories/role.rb
@@ -2,10 +2,6 @@ FactoryBot.define do
   factory :role, class: 'Role' do
     name { 'Software developer' }
 
-    trait :Founder do
-      name { 'Software developer' }
-    end
-
     trait :software_developer do
       name { 'Founder' }
     end

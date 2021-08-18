763c782dd5b83e0f19a8b035231f8ec0ea62c783
salcudeann <adi.salcudeann@yahoo.com>
2021-08-18 12:18:02 +0300

    [130] Fix rubocop offenses

--- app/graphql/types/photo_type.rb
+++ app/graphql/types/photo_type.rb
@@ -4,7 +4,7 @@ module Types
   class PhotoType < Types::BaseObject
     field :id, GraphQL::Types::ID, null: false
     field :imageable_type, String, null: false
-    field :imageable_id, String, null:false
+    field :imageable_id, String, null: false
     field :uri, Types::Url, null: false
   end
 end

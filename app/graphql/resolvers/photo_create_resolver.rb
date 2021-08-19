523c7e86f1d8d1cfdd76cd225405c0f24063175e
salcudeann <adi.salcudeann@yahoo.com>
2021-08-19 15:07:54 +0300

    [130] Add description in a correct way for imageable_type field

--- app/graphql/resolvers/photo_create_resolver.rb
+++ app/graphql/resolvers/photo_create_resolver.rb
@@ -3,8 +3,8 @@
 module Resolvers
   class PhotoCreateResolver < Resolvers::Base
     argument :imageable_id, GraphQL::Types::ID, required: true
-    # imageable_type can be Freak or Project, depending where the photo is assigned
-    argument :imageable_type, String, required: true
+    argument :imageable_type, String, 'Can have value Freak or Project, depending where the photo is assigned',
+             required: true
     argument :uri, Types::Url, required: true
 
     type [Types::PhotoType], null: false

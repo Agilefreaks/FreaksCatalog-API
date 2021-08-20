f2ef7464a87b1f747025d42dcf7ecd4e904866df
salcudeann <adi.salcudeann@yahoo.com>
2021-08-20 14:50:42 +0300

    [120] Remove unnecesarry relation for Technology model

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -1,6 +1,6 @@
-class Technology < ApplicationRecord
-  belongs_to :freak
+# frozen_string_literal: true
 
+class Technology < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
 end

1a1c0d21a3a2dd435c43a7c24394ffa2c1a347ac
gabrielgiroe <gabriel.giroe@gmail.com>
2021-08-22 15:55:24 +0300

    [119] Add migration and create a levels table

---
+++ db/migrate/20210822124413_create_level.rb
@@ -0,0 +1,15 @@
+class CreateLevel < ActiveRecord::Migration[6.1]
+  def up
+    create_table :levels do |t|
+      t.string :name
+      t.timestamps
+    end
+    add_column :freaks, :level_id, :bigint
+    add_foreign_key :freaks, :levels
+  end
+
+  def down
+    remove_column :freaks,:level_id
+    drop_table :levels
+  end
+end

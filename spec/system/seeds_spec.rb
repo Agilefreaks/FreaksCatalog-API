f4f96c2eebbb5fef70d17fb085a4e0b55947d65d
pirvudoru <pirvu.doru@ymail.com>
2021-08-23 11:23:47 +0300

    Add seeds spec

---
+++ spec/system/seeds_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require './db/seeds'
+
+RSpec.describe Seeds do
+  let(:action) { -> { described_class.run } }
+
+  subject { action.call }
+
+  it 'populates the entities when called multiple times without polluting' do
+    def counts
+      seeded_types = [
+        Freak,
+        Technology
+      ]
+
+      seeded_types.each_with_object({}) do |type, result|
+        result[type.to_s] = type.count
+      end
+    end
+
+    action.call
+    counts.each do |type, count|
+      expect(count).not_to eq(0), "Expected seed task to create #{type} items. It didn't..."
+    end
+
+    # second call does not pollute
+    expect { action.call }.not_to change { counts }
+  end
+end

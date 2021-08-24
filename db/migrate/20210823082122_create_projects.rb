5e0eae72369239b2117e346717af48fe16c98700
salcudeann <adi.salcudeann@yahoo.com>
2021-08-24 10:00:20 +0300

    [121] Create migration for projects table

---
+++ db/migrate/20210823082122_create_projects.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateProjects < ActiveRecord::Migration[6.1]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end

fc24080f89f0b0f956e81e1743701408576aed20
salcudeann <adi.salcudeann@yahoo.com>
2021-08-24 10:01:53 +0300

    [121] Create Project model

---
+++ app/models/project.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Project < ApplicationRecord
+  has_one :logoUrl, as: :imageable, dependent: :destroy, class_name: 'Photo'
+
+  has_many :freaks_projects, dependent: nil, class_name: 'FreakProject'
+  has_many :freaks, through: :freaks_projects, dependent: nil
+
+  has_many :projects_technologies, dependent: nil, class_name: 'ProjectTechnology'
+  has_many :technologies, through: :projects_technologies
+
+  validates :name, presence: true
+  validates :description, presence: true
+end

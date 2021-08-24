b1892d7e4df3aee57d932351aa24e15da1e789aa
gabrielgiroe <gabriel.giroe@gmail.com>
2021-08-24 16:15:43 +0300

    [117] Fic rubocop offenses

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,9 +12,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_820_054_103) do
+ActiveRecord::Schema.define(version: 2021_08_16_125701) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
   create_table 'freaks', force: :cascade do |t|
     t.string 'description'
@@ -24,14 +27,14 @@ ActiveRecord::Schema.define(version: 20_210_820_054_103) do
     t.bigint 'norm_id'
   end
 
-  create_table "freaks", force: :cascade do |t|
-    t.string "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "norm_id"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
+  create_table 'freaks', force: :cascade do |t|
+    t.string 'description'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.bigint 'norm_id'
+    t.string 'first_name'
+    t.string 'last_name'
+    t.string 'email'
   end
 
   create_table 'freaks_projects', force: :cascade do |t|
@@ -52,10 +55,10 @@ ActiveRecord::Schema.define(version: 20_210_820_054_103) do
     t.index ['technology_id'], name: 'index_freaks_technologies_on_technology_id'
   end
 
-  create_table "norms", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'norms', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
   create_table 'projects', force: :cascade do |t|
@@ -86,14 +89,14 @@ ActiveRecord::Schema.define(version: 20_210_820_054_103) do
   add_foreign_key 'projects_technologies', 'projects'
   add_foreign_key 'projects_technologies', 'technologies'
 
-  create_table "photos", force: :cascade do |t|
-    t.string "uri"
-    t.string "imageable_type"
-    t.bigint "imageable_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["imageable_type", "imageable_id"], name: "index_photos_on_imageable"
+  create_table 'photos', force: :cascade do |t|
+    t.string 'uri'
+    t.string 'imageable_type'
+    t.bigint 'imageable_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index %w[imageable_type imageable_id], name: 'index_photos_on_imageable'
   end
 
-  add_foreign_key "freaks", "norms"
+  add_foreign_key 'freaks', 'norms'
 end

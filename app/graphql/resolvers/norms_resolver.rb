23302c3afcfeafd83bca91776155d963603b6c1a
gabrielgiroe <gabriel.giroe@gmail.com>
2021-08-25 12:11:35 +0300

    [117] Refactoring the code

--- app/graphql/resolvers/norms_resolver.rb
+++ app/graphql/resolvers/norms_resolver.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
-require 'json'
-
 module Resolvers
   class NormsResolver < Resolvers::Base
     type Types::NormType.connection_type, null: false
 
-    # :reek:UtilityFunction
     def resolve
       Norm.all
     end

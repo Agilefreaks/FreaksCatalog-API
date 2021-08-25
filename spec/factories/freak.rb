b7ce9da0de40e5cdac5e5907ab1fcfb742c5a1d1
salcudeann <adi.salcudeann@yahoo.com>
2021-08-25 09:59:46 +0300

    [142] Create :with_project trait for freaks

--- spec/factories/freak.rb
+++ spec/factories/freak.rb
@@ -7,10 +7,15 @@ FactoryBot.define do
     description { 'a simple freak' }
     email { 'freak@gmail.com' }
 
+    trait :with_project do
+      after(:build) do |freak|
+        freak.projects = create_list(:project, 1)
+      end
+    end
+
     after(:build) do |freak|
       freak.photo = create(:photo, imageable: freak)
       freak.technologies = create_list(:technology, 1, :ruby)
-      freak.projects = create_list(:project, 1)
     end
   end
 end

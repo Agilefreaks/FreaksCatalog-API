c79098e02396cc0248a1955ea1a576ad43da11e4
pirvudoru <pirvu.doru@ymail.com>
2021-08-30 14:41:36 +0300

    [118] Parameterize query

--- spec/controllers/graphql/mutations/freak_create_spec.rb
+++ spec/controllers/graphql/mutations/freak_create_spec.rb
@@ -4,26 +4,20 @@ require 'rails_helper'
 
 module Graphql
   RSpec.describe GraphqlController, type: :controller do
-    subject { post :execute, params: params, as: :json }
-
+    let(:norm) { create(:norm) }
     let(:params) do
       {
-        query: File.read('spec/fixtures/requests/mutations/freak_create.graphql')
+        query: File.read('spec/fixtures/requests/mutations/freak_create.graphql'),
+        variables: {
+          norm_id: norm.id,
+          role_id: role.id
+        }
       }
     end
+    let(:role) { create(:role) }
 
-    context 'when norm and role exists' do
-      before do
-        create(:freak, :with_project)
-      end
+    subject { post :execute, params: params, as: :json }
 
-      it 'sgeqge' do
-         subject
-         # byebug
-        # response.body
-      end
-
-      it { is_expected.to match_response_for(mutation: :freak_create, sample: :freak_create) }
-    end
+    it { is_expected.to match_response_for(mutation: :freak_create, sample: :freak_create) }
   end
 end

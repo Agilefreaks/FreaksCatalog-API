bef9afe931b0cfe1e3f37ef47f3dbae035362556
gabrielgiroe <gabriel.giroe@gmail.com>
2021-08-31 15:10:48 +0300

    [145] Add freak update resolver with some issue

---
+++ app/graphql/resolvers/freak_update_resolver.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Resolvers
+  class FreakUpdateResolver < Resolvers::Base
+    argument :id, GraphQL::Types::ID, required: true
+    argument :first_name, String, required: true
+    argument :last_name, String, required: true
+    argument :description, String, required: true
+    argument :email, String, required: true
+    argument :norm_id, GraphQL::Types::ID, required: true
+    argument :role_id, GraphQL::Types::ID, required: true
+    argument :level_id, GraphQL::Types::ID, required: true
+
+    type [Types::FreakType], null: false
+
+    # TODO: Test if norm_id exist
+    # TODO: Test if role_id exist
+
+    def resolve(params)
+
+      norm = Norm.find(params[:norm_id])
+      role = Role.find(params[:role_id])
+      level = Level.find(params[:level_id])
+
+      Freak.where(id: params[:id]).update(params.merge(norm: norm, role: role, level: level))
+    end
+  end
+end

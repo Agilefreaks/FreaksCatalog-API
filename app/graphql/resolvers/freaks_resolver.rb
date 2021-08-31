f9f6b2435cd2b18d6ff65d1ca0bad233fa01d92d
salcudeann <adi.salcudeann@yahoo.com>
2021-08-31 10:58:50 +0300

    [143] Modify freaks resolver

--- app/graphql/resolvers/freaks_resolver.rb
+++ app/graphql/resolvers/freaks_resolver.rb
@@ -2,11 +2,22 @@
 
 module Resolvers
   class FreaksResolver < Resolvers::Base
+    argument :filter, Types::FreakFilterType, required: false
+
     type Types::FreakType.connection_type, null: false
 
     # :reek:UtilityFunction
-    def resolve
-      Freak.all
+    def resolve(args)
+      all_of_project_ids = args.dig(:filter, :project_ids, :all_of)
+
+      query = Freak.all
+
+      if all_of_project_ids.present?
+        query = query.joins(:freaks_projects).having('array_agg(freaks_projects.project_id) @> ARRAY[:ids]::bigint[]',
+                                                     ids: all_of_project_ids).group(:id)
+      end
+
+      query
     end
   end
 end

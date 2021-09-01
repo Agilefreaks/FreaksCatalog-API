c23cfc79b05f44de9b33a7406c1ed532d1f74a68
salcudeann <adi.salcudeann@yahoo.com>
2021-09-01 11:11:49 +0300

    [143] Add validation for all_of and any_of technologies filter

--- app/graphql/types/technology_id_filter_type.rb
+++ app/graphql/types/technology_id_filter_type.rb
@@ -4,5 +4,7 @@ module Types
   class TechnologyIdFilterType < Types::BaseInputObject
     argument :all_of, [GraphQL::Types::ID], required: false
     argument :any_of, [GraphQL::Types::ID], required: false
+
+    validates required: { one_of: %i[all_of any_of], message: 'May use either allOf  or anyOf, but not both.' }
   end
 end

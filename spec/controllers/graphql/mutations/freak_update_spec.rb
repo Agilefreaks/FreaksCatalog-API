f260f78107b07fe73aa9875fa7fdf880db9aa0d6
gabrielgiroe <gabriel.giroe@gmail.com>
2021-09-01 10:56:56 +0300

    [145] Add a test for freak update

---
+++ spec/controllers/graphql/mutations/freak_update_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module Graphql
+  RSpec.describe GraphqlController, type: :controller do
+    subject { post :execute, params: params, as: :json }
+
+    let(:norm) { create(:norm) }
+    let(:params) do
+      {
+        query: File.read('spec/fixtures/requests/mutations/freak_create.graphql'),
+        variables: {
+          norm_id: norm.id,
+          role_id: role.id,
+          level_id: level.id
+        }
+      }
+    end
+
+    let(:role) { create(:role) }
+    let(:level) { create(:level) }
+
+    it { is_expected.to match_response_for(mutation: :freak_update, sample: :freak_update) }
+  end
+end

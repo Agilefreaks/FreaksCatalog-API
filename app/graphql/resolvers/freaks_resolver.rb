d74ba043a2b37fcd786a6b162cb9b311d7dd02ec
salcudeann <adi.salcudeann@yahoo.com>
2021-09-01 09:17:13 +0300

    [143] Add any_off projects in freaks resolver

--- app/graphql/resolvers/freaks_resolver.rb
+++ app/graphql/resolvers/freaks_resolver.rb
@@ -8,16 +8,26 @@ module Resolvers
 
     # :reek:UtilityFunction
     def resolve(filter: nil)
-      query = Freak.all
 
       all_of_project_ids = filter&.dig(:project_ids, :all_of)
+      any_of_project_ids = filter&.dig(:project_ids, :any_of)
+
+      query = Freak.all if filter.nil?
 
       if all_of_project_ids.present?
-        query = query
-                .joins(:freaks_projects)
-                .having('array_agg(freaks_projects.project_id) @> ARRAY[:ids]::bigint[]', ids: all_of_project_ids)
-                .group(:id)
-                .order(:id)
+        query = Freak
+                  .joins(:freaks_projects)
+                  .having('array_agg(freaks_projects.project_id) @> ARRAY[:ids]::bigint[]', ids: all_of_project_ids)
+                  .group(:id)
+                  .order(:id)
+      end
+
+      if any_of_project_ids.present?
+        query = Freak
+                  .joins(:freaks_projects)
+                  .where('freaks_projects.project_id = ANY(ARRAY[:ids]::bigint[])', ids: any_of_project_ids)
+                  .group(:id)
+                  .order(:id)
       end
 
       query

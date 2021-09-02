e977ba5255d07d90277c6c4604c626f9009b46d5
gabrielgiroe <gabriel.giroe@gmail.com>
2021-09-02 09:25:18 +0300

    [145] Add a test for freak update

--- spec/controllers/graphql/mutations/freak_update_spec.rb
+++ spec/controllers/graphql/mutations/freak_update_spec.rb
@@ -7,27 +7,29 @@ module Graphql
     subject { post :execute, params: params, as: :json }
 
     let(:norm) { create(:norm) }
-    let(:freak){create(:freak)}
+    let(:role) { create(:role) }
+    let(:level) { create(:level) }
+    let(:freak) { create(:freak) }
     let(:params) do
       {
         query: File.read('spec/fixtures/requests/mutations/freak_update.graphql')
 
       }
     end
+
     before do
       # create(:freak)
-      params[:variables]={
+      params[:variables] = {
         id: freak.id,
-        firstName: "matheo",
-        lastName: "ion",
+        firstName: freak.first_name,
+        lastName: freak.last_name,
+        description: freak.description,
+        email: freak.email,
         norm_id: norm.id,
         role_id: role.id,
         level_id: level.id
       }
     end
-
-    let(:role) { create(:role) }
-    let(:level) { create(:level) }
 
     it { is_expected.to match_response_for(mutation: :freak_update, sample: :freak_update) }
   end

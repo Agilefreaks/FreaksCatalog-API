39f3f28d68a717dd8af04b13942925596b3f5e88
salcudeann <adi.salcudeann@yahoo.com>
2021-09-02 12:55:56 +0300

    [] Add cors.rb

---
+++ config/initializers/cors.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rack/cors'
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', headers: :any, methods: :any
+  end
+end

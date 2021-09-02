44941941a00537d9c99b170e8a9c33fb27e59894
Doru Pirvu <pirvu.doru@ymail.com>
2021-09-02 12:43:34 +0300

    Fix Rubocop

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,8 +3,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*', 
-             headers: :any, 
+    resource '*',
+             headers: :any,
              methods: %i[get post put patch delete options head]
   end
 end

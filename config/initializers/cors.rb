9925b754c4c32630795ca193e154b84d2b7804a7
salcudeann <adi.salcudeann@yahoo.com>
2021-09-02 12:27:15 +0300

    [] Fix rubocop offenses

--- config/initializers/cors.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    resource '*', headers: :any, methods: :any
-  end
-
-end

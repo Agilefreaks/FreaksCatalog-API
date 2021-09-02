15ef12dec7a1caf8215792a9db61eaf12bf00c43
Doru Pirvu <pirvu.doru@ymail.com>
2021-09-02 12:44:25 +0300

    Use any methods

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
     resource '*',
              headers: :any,
-             methods: %i[get post put patch delete options head]
+             methods: :any
   end
 end

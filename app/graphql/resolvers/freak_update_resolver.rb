428fc0f6d97c0cef884e4aeb264108a52de66c7f
gabrielgiroe <gabriel.giroe@gmail.com>
2021-09-07 14:23:41 +0300

    [213] Refactoring freak update resolver

--- app/graphql/resolvers/freak_update_resolver.rb
+++ app/graphql/resolvers/freak_update_resolver.rb
@@ -2,24 +2,16 @@
 
 module Resolvers
   class FreakUpdateResolver < Resolvers::Base
-    argument :id, GraphQL::Types::ID, required: true
-    argument :first_name, String, required: false
-    argument :last_name, String, required: false
-    argument :description, String, required: false
-    argument :email, String, required: false
-    argument :norm_id, GraphQL::Types::ID, required: false
-    argument :role_id, GraphQL::Types::ID, required: false
-    argument :level_id, GraphQL::Types::ID, required: false
+    argument :input, Types::FreakUpdateInputType, required: true
 
     type Types::FreakType, null: false
 
-    # TODO: Test if Freak with this id exist. Test if norm_id,level_id and role_id exist
-    def resolve(id:, **attrs)
-      freak = Freak.find(id)
+    def resolve(input:)
+      freak = Freak.find(input[:id])
 
       return unless freak
 
-      freak.update(attrs)
+      freak.update(input.to_h)
       freak
     end
   end

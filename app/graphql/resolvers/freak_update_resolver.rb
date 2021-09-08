923483a81bec7d7687a7919aac119b7028edb855
salcudeann <59455264+salcudeann@users.noreply.github.com>
2021-09-08 11:52:55 +0300

    [] Move arguments in resolvers (#47)
    * [] Move arguments from freak_create_input_type to freak_create_resolver
    
    * [] Remove input argument from freakCreate mutation
    
    * [] Adapt expected spec respones for latest freakCreate mutation
    
    * [] Move arguments from freak_update_input_type to freak_update_resolver
    
    * [] Update freak_update_resolver
    
    * [] Remove input argument from freak_update mutation

--- app/graphql/resolvers/freak_update_resolver.rb
+++ app/graphql/resolvers/freak_update_resolver.rb
@@ -2,16 +2,27 @@
 
 module Resolvers
   class FreakUpdateResolver < Resolvers::Base
-    argument :input, Types::FreakUpdateInputType, required: true
+    argument :id, GraphQL::Types::ID, required: true,
+                                      validates: { in_db: { type: Freak } }
+    argument :first_name, String, required: false
+    argument :last_name, String, required: false
+    argument :description, String, required: false
+    argument :email, String, required: false
+    argument :norm_id, GraphQL::Types::ID, required: false,
+                                           validates: { in_db: { type: Norm } }
+    argument :role_id, GraphQL::Types::ID, required: false,
+                                           validates: { in_db: { type: Role } }
+    argument :level_id, GraphQL::Types::ID, required: false,
+                                            validates: { in_db: { type: Level } }
 
     type Types::FreakType, null: false
 
-    def resolve(input:)
-      freak = Freak.find(input[:id])
+    def resolve(id:, **attrs)
+      freak = Freak.find(id)
 
       return unless freak
 
-      freak.update(input.to_h)
+      freak.update(attrs)
       freak
     end
   end

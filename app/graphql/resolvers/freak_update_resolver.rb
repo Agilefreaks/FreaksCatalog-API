a2885477418808c3c35c37a4e5c6da7954001592
salcudeann <adi.salcudeann@yahoo.com>
2021-09-08 10:53:39 +0300

    [] Update freak_update_resolver

--- app/graphql/resolvers/freak_update_resolver.rb
+++ app/graphql/resolvers/freak_update_resolver.rb
@@ -3,26 +3,26 @@
 module Resolvers
   class FreakUpdateResolver < Resolvers::Base
     argument :id, GraphQL::Types::ID, required: true,
-             validates: { in_db: { type: Freak } }
+                                      validates: { in_db: { type: Freak } }
     argument :first_name, String, required: false
     argument :last_name, String, required: false
     argument :description, String, required: false
     argument :email, String, required: false
     argument :norm_id, GraphQL::Types::ID, required: false,
-             validates: { in_db: { type: Norm } }
+                                           validates: { in_db: { type: Norm } }
     argument :role_id, GraphQL::Types::ID, required: false,
-             validates: { in_db: { type: Role } }
+                                           validates: { in_db: { type: Role } }
     argument :level_id, GraphQL::Types::ID, required: false,
-             validates: { in_db: { type: Level } }
+                                            validates: { in_db: { type: Level } }
 
     type Types::FreakType, null: false
 
-    def resolve(input:)
-      freak = Freak.find(input[:id])
+    def resolve(id:, **attrs)
+      freak = Freak.find(id)
 
       return unless freak
 
-      freak.update(input.to_h)
+      freak.update(attrs)
       freak
     end
   end

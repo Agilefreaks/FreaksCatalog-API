c26be17ccb99b69f52d7e724d41ed01d2a4d88d5
gabrielgiroe1 <83423182+gabrielgiroe1@users.noreply.github.com>
2021-09-08 09:51:18 +0300

    [213] Add validations for freakupdate (#46)
    * [180] Add contracts file and add gem for validation
    
    * [180] Remove contract for freak_create
    
    * [180] Remove contract call from freak_create_resolver
    
    * [180] Remove dry-validation gem
    
    * [180] Add a freak_create validator
    
    * [180] Move arguments for freak_create in a separate type
    
    * [180] Refactor validator
    
    * [180] Modify freak_create resolver
    
    * [180] Update freak_create mutation
    
    * [180] Create a spec when norm does not exist.
    
    * [180] Refactor code
    
    * [180] Modify return type for photo_create_resolver
    
    * [180] Fix rubocop offenses.
    
    * [180] Fix update_freak spec issues after rebasing
    
    * [180] Add validation for role_id
    
    * [180] Create a spec when role does not exist.
    
    * [180] Add validation for level_id.
    
    * [180] Create a spec when level does not exist.
    
    * [180] Fix rubocop offenses.
    
    * [213] Add input type for freak update and add validation for id, norm_id, role_id, level_id
    
    * [213] Delete application_contract
    
    * [213] Refactoring freak update resolver
    
    * [213] Add test to see if validation work
    
    * [213] Resolve test
    
    Co-authored-by: salcudeann <adi.salcudeann@yahoo.com>

--- spec/controllers/graphql/mutations/freak_update_spec.rb
+++ spec/controllers/graphql/mutations/freak_update_spec.rb
@@ -6,27 +6,51 @@ module Graphql
   RSpec.describe GraphqlController, type: :controller do
     subject { post :execute, params: params, as: :json }
 
-    let(:norm) { create(:norm) }
-    let(:role) { create(:role) }
-    let(:level) { create(:level) }
-    let(:freak) { create(:freak) }
+    let(:norm_id) { create(:norm).id }
+    let(:role_id) { create(:role).id }
+    let(:level_id) { create(:level).id }
+    let(:freak_id) { create(:freak).id }
     let(:params) do
       {
         query: File.read('spec/fixtures/requests/mutations/freak_update.graphql'),
         variables: {
-          id: freak.id,
+          id: freak_id,
           firstName: 'Dorel',
           lastName: 'zidarul',
           description: 'priceput la tot prin casa',
           email: 'dorel.zidarul@gmail.com',
-          norm_id: norm.id,
-          role_id: role.id,
-          level_id: level.id
+          norm_id: norm_id,
+          role_id: role_id,
+          level_id: level_id
         }
 
       }
     end
 
     it { is_expected.to match_response_for(mutation: :freak_update, sample: :freak_update) }
+
+    context 'when freak does not exist' do
+      let(:freak_id) { 231_231 }
+
+      it { is_expected.to match_response_for(mutation: :freak_update, sample: :missing_id) }
+    end
+
+    context 'when norm does not exist' do
+      let(:norm_id) { 8551 }
+
+      it { is_expected.to match_response_for(mutation: :freak_update, sample: :missing_norm) }
+    end
+
+    context 'when role does not exist' do
+      let(:role_id) { 8551 }
+
+      it { is_expected.to match_response_for(mutation: :freak_update, sample: :missing_role) }
+    end
+
+    context 'when level does not exist' do
+      let(:level_id) { 232_323 }
+
+      it { is_expected.to match_response_for(mutation: :freak_update, sample: :missing_level) }
+    end
   end
 end
